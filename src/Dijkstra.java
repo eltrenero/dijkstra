3861e2ee20a3655af71d6bcd9085f593a92e1360
eltrenero <elteomo@gmail.com>
2017-10-21 21:06:25 +0200

    second onegit add src bin .settings/ .project .classpath

---
+++ src/Dijkstra.java
@@ -0,0 +1,8 @@
+public class Dijkstra {
+public static void main(String args[]) {
+	int[] daro= {1,2,3,4,5};
+	for(int i:daro) {
+	System.out.println(i);
+	}
+}
+}
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Node {
+    public String name;
+    public List<Edge> connections;
+    
+    Node(String name, List<Edge> connections){
+    	this.name=name;
+    	this.connections=connections;
+    }
+}
